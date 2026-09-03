99581d015564ccbec8694c33d851383c52974a1d
Igor <igormcetec@gmail.com>
2026-09-03 20:45:29 -0300

    feat: Criando a classe Doces

---
+++ LojaDoces/Doces.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace LojaDoces
+{
+    class Doces
+    {
+        //PROPRIEDADES(ATRIBUTOS)
+        public string? Confeiteiro {  get; set; }
+        public double CapacidadeKg { get; set; }
+        public double PorcentagemPronta { get; set; }
+        public double TaxaProdutoPorHora {  get; set; }
+
+        public double CalcularKgFaltantes()
+        {
+            double porcentagemFaltate = (100.0 - PorcentagemPronta) / 100.0;
+            return CapacidadeKg * porcentagemFaltate;
+        }
+
+        public double CalcularTempoRestanteHoras() 
+        {
+            return CalcularTempoRestanteHoras() / TaxaProdutoPorHora;
+        }
+
+        public double CalcularValorFaltante() 
+        { 
+            const double PRECO_POR_KG = 45.50;
+            return CalcularKgFaltantes() * PRECO_POR_KG;
+        }
+        
+    }
+}
